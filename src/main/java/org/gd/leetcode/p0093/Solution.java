5c2eb2730f62aff72c7962018a6fdedb525635bc
DimkaGorhover <gd.mail.89@gmail.com>
2018-10-23 23:07:28 +0300

    add TODOs: p0721 & p0093

---
+++ src/main/java/org/gd/leetcode/p0093/Solution.java
@@ -0,0 +1,16 @@
+package org.gd.leetcode.p0093;
+
+import java.util.List;
+
+/**
+ * TODO: https://leetcode.com/problems/restore-ip-addresses/
+ *
+ * @author Gorkhover D.
+ * @since 2018-10-23
+ */
+class Solution {
+
+    public List<String> restoreIpAddresses(String s) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/org/gd/leetcode/p0721/Solution.java
@@ -0,0 +1,16 @@
+package org.gd.leetcode.p0721;
+
+import java.util.List;
+
+/**
+ * TODO: https://leetcode.com/problems/accounts-merge/
+ *
+ * @author Gorkhover D.
+ * @since 2018-10-23
+ */
+class Solution {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+
+    }
+}
