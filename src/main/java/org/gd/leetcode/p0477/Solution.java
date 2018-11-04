da3f6d310741fe4f567f6aed0e60c2e72ddade28
DimkaGorhover <gd.mail.89@gmail.com>
2018-11-05 00:53:55 +0200

    p0477 fix :(

--- src/main/java/org/gd/leetcode/p0477/Solution.java
+++ src/main/java/org/gd/leetcode/p0477/Solution.java
@@ -10,24 +10,9 @@ package org.gd.leetcode.p0477;
  */
 class Solution {
 
-    static int bits(int r) {
-        if (r == 0) return 0;
-        if (r < Byte.MAX_VALUE) return Byte.SIZE;
-        if (r < Short.MAX_VALUE) return Short.SIZE;
-        return Integer.SIZE;
-    }
-
-    static int hammingDistance(int i1, int i2) {
-        int r = i1 ^ i2,
-                length = 0;
-        for (int i = 0; i < bits(r); i++)
-            length += (r >> i) & 1;
-        return length;
-    }
-
     public int totalHammingDistance(int[] nums) {
         int total = 0, n = nums.length;
-        for (int j = 0; j < Integer.SIZE - 1; j++) {
+        for (int j = 0; j < Integer.SIZE; j++) {
             int bitCount = 0;
             for (int i = 0; i < n; i++)
                 bitCount += (nums[i] >> j) & 1;
