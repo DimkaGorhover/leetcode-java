81cf6d211e909c12a4caff5f51b74a9d632c57c1
Dmytro Horkhover <ext-dmytro.horkhover@here.com>
2018-11-19 10:40:37 +0200

    LeetCode 334 todo

---
+++ src/main/java/org/gd/leetcode/p0334/Solution.java
@@ -0,0 +1,14 @@
+package org.gd.leetcode.p0334;
+
+/**
+ * TODO: https://leetcode.com/problems/increasing-triplet-subsequence/
+ *
+ * @author Horkhover Dmytro
+ * @since 2018-11-19
+ */
+class Solution {
+
+    public boolean increasingTriplet(int[] nums) {
+        throw new UnsupportedOperationException();
+    }
+}
