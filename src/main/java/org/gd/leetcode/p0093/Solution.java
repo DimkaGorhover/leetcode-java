e9d3427d9f254fd09fb861fb2b82d625961dd301
Dmytro Horkhover <gd.mail.89@google.com>
2018-12-11 17:32:09 +0200

    LeetCode 93: dev state

--- src/main/java/org/gd/leetcode/p0093/Solution.java
+++ src/main/java/org/gd/leetcode/p0093/Solution.java
@@ -1,5 +1,7 @@
 package org.gd.leetcode.p0093;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,7 +12,70 @@ import java.util.List;
  */
 class Solution {
 
+    Solution() {}
+
+    private static int v0(String s, int index) { return s.charAt(index) - 48; }
+
+    private static boolean isv(int value) { return 0 <= value && value <= 255;}
+
+    private static int value(String s, int start, int end) {
+        switch ((end = Math.min(end, s.length())) - start) {
+            case 0: return 0;
+            case 1: return v0(s, start);
+            case 2: return v0(s, start) * 10 + v0(s, start + 1);
+            case 3: return v0(s, start) * 100 + v0(s, start + 1) * 10 + v0(s, start + 2);
+        }
+        int sum = 0;
+        for (int i = start; i < end; i++)
+            sum = sum * 10 + v0(s, i);
+        return sum;
+    }
+
+    private static List<String> restoreIpAddresses(List<String> list,
+                                                   String source,
+                                                   String prefix,
+                                                   int sourcePos,
+                                                   int ipPos) {
+        if (ipPos == 3) {
+            final int value = value(source, sourcePos, source.length());
+            if (isv(value))
+                list.add(prefix + "." + value);
+        } else {
+
+            int min = (3 - ipPos);
+            int max = (3 - ipPos) * 3;
+
+            int remains = source.length() - sourcePos;
+
+
+        }
+
+
+        return list;
+    }
+
     public List<String> restoreIpAddresses(String s) {
-        throw new UnsupportedOperationException();
+        switch (s.length()) {
+            case 12: {
+                final int[] ip = new int[4];
+                if (isv(ip[0] = value(s, 0, 3))
+                        && isv(ip[1] = value(s, 3, 6))
+                        && isv(ip[2] = value(s, 6, 9))
+                        && isv(ip[3] = value(s, 9, 12)))
+                    return Collections.singletonList(ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3]);
+                return Collections.emptyList();
+            }
+            case 4: {
+                final int[] ip = new int[4];
+                if (isv(ip[0] = v0(s, 0))
+                        && isv(ip[1] = v0(s, 1))
+                        && isv(ip[2] = v0(s, 2))
+                        && isv(ip[3] = v0(s, 3)))
+                    return Collections.singletonList(ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3]);
+                return Collections.emptyList();
+            }
+        }
+
+        return restoreIpAddresses(new ArrayList<>(), s, "", 0, 0);
     }
 }
---
+++ src/test/java/org/gd/leetcode/p0093/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.gd.leetcode.p0093;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution};
+ *
+ * @author Horkhover Dmytro
+ * @since 2018-12-11
+ */
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.arguments("111111111111", List.of("111.111.111.111")),
+                Arguments.arguments("1111", List.of("1.1.1.1"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("RestoreIpAddresses")
+    void test_RestoreIpAddresses(String text, List<String> expected) {
+        assertEquals(
+                expected,
+                new Solution().restoreIpAddresses(text)
+        );
+    }
+}
