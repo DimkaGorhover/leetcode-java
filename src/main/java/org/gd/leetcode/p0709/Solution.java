3d6a368839a27be915d08e9e95ab53a240889540
Dmytro Horkhover <gd.mail.89@google.com>
2018-12-18 20:14:23 +0200

    LeetCode 709 done =)

---
+++ src/main/java/org/gd/leetcode/p0709/Solution.java
@@ -0,0 +1,11 @@
+package org.gd.leetcode.p0709;
+
+/**
+ * @author Horkhover Dmytro
+ * @since 2018-12-18
+ */
+class Solution {
+    public String toLowerCase(String str) {
+        return str == null || str.isEmpty() ? str : str.toLowerCase();
+    }
+}
