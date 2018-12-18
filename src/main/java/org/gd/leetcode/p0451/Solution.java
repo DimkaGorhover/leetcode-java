4de4d9acfa8238ec9ce870d138c0aa589c318409
Dmytro Horkhover <gd.mail.89@google.com>
2018-12-18 15:29:09 +0200

    LeetCode 451: slow but done

---
+++ src/main/java/org/gd/leetcode/p0451/Solution.java
@@ -0,0 +1,36 @@
+package org.gd.leetcode.p0451;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ *
+ * @author Horkhover Dmytro
+ * @since 2018-12-18
+ */
+class Solution {
+
+    public String frequencySort(String s) {
+
+        final int         l      = s.length();
+        final int[]       counts = new int[128];
+        final Character[] chars0 = new Character[l];
+        final char[]      chars1 = new char[l];
+
+        for (int i = 0; i < l; i++) {
+            char c = s.charAt(i);
+            counts[c]++;
+            chars0[i] = c;
+        }
+
+        Arrays.sort(chars0, 0, chars0.length, (o1, o2) -> {
+            final int compare = counts[o2] - counts[o1];
+            return compare != 0 ? compare : o1 - o2;
+        });
+
+        for (int i = 0; i < chars0.length; i++)
+            chars1[i] = chars0[i];
+
+        return new String(chars1);
+    }
+}
