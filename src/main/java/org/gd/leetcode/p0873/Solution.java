e21b667f4753125edcf531f004dc670d73c78617
Dmytro Horkhover <gd.mail.89@google.com>
2018-12-18 11:23:52 +0200

    LeetCode 873: binary search -> hash

--- src/main/java/org/gd/leetcode/p0873/Solution.java
+++ src/main/java/org/gd/leetcode/p0873/Solution.java
@@ -1,6 +1,6 @@
 package org.gd.leetcode.p0873;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/
@@ -12,11 +12,12 @@ class Solution {
 
     private static int find(int[] A, int from,
                             int a1, int a2,
+                            HashMap<Integer, Integer> index,
                             int fibLength) {
         final int f = a1 + a2, last = A[A.length - 1], pos;
         if (f == last) return fibLength + 1;
-        return (f <= last && (pos = Arrays.binarySearch(A, from, A.length, f)) > 0)
-                ? find(A, pos + 1, a2, A[pos], fibLength + 1)
+        return (f <= last && (pos = index.getOrDefault(f, -1)) > 0)
+                ? find(A, pos + 1, a2, A[pos], index, fibLength + 1)
                 : fibLength;
     }
 
@@ -30,10 +31,15 @@ class Solution {
             case 2: return 0;
             case 3: return (A[0] + A[1] == A[2]) ? 3 : 0;
         }
+
+        HashMap<Integer, Integer> index = new HashMap<>(A.length - 2);
+        for (int i = 2; i < A.length; i++)
+            index.put(A[i], i);
+
         int max = 0;
         for (int i = 0; i < length - 2; i++)
             for (int j = i + 1; j < length - 1; j++)
-                max = Math.max(max, find(A, j + 1, A[i], A[j], 0));
+                max = Math.max(max, find(A, j + 1, A[i], A[j], index, 0));
         return max > 0 ? max + 2 : 0;
     }
 }
