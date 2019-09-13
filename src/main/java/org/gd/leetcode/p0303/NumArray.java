ad9435dcf7b9a17ce2e21377414ea385b0ba13b1
Dmytro_Horkhover <gd.mail.89@gmail.com>
2019-09-13 16:42:17 +0300

    LeetCode 303

---
+++ src/main/java/org/gd/leetcode/p0303/NumArray.java
@@ -0,0 +1,24 @@
+package org.gd.leetcode.p0303;
+
+import org.gd.leetcode.common.LeetCode;
+
+/**
+ * https://leetcode.com/problems/range-sum-query-immutable/
+ *
+ * @since 2019-09-13
+ */
+@LeetCode(difficulty = LeetCode.Level.EASY, tags = {LeetCode.Tags.DYNAMIC_PROGRAMMING})
+class NumArray {
+
+    private final int[] sums;
+
+    public NumArray(int[] nums) {
+        sums = new int[nums.length];
+        for (int i = 0, sum = 0; i < nums.length; i++)
+            sums[i] = (sum += nums[i]);
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j] - (i == 0 ? 0 : sums[i - 1]);
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0303/NumArrayTest.java
@@ -0,0 +1,83 @@
+package org.gd.leetcode.p0303;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.annotation.Nonnull;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link NumArray}
+ *
+ * @since 2019-09-13
+ */
+class NumArrayTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                new Case(new int[]{-2, 0, 3, -5, 2, -1}, List.of(
+                        new SubCase(0, 2, 1),
+                        new SubCase(2, 5, -1),
+                        new SubCase(0, 5, -3)
+                ))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("SumRange")
+    void test_SumRange(Case aCase) throws Exception {
+
+        NumArray numArray = new NumArray(aCase.nums);
+        for (SubCase subCase : aCase) {
+            assertEquals(subCase.expected, numArray.sumRange(subCase.i, subCase.j), subCase::toString);
+        }
+    }
+
+    static class Case implements Iterable<SubCase>, Arguments {
+
+        private final int[] nums;
+        private final List<SubCase> subCases;
+
+        Case(int[] nums, List<SubCase> subCases) {
+
+            requireNonNull(nums, "\"nums\" cannot be null");
+            requireNonNull(subCases, "\"subCases\" cannot be null");
+
+            this.nums = nums;
+            this.subCases = subCases;
+        }
+
+        @Nonnull
+        @Override
+        public Iterator<SubCase> iterator() {
+            return subCases.iterator();
+        }
+
+        @Override
+        public Object[] get() { return new Object[]{this}; }
+    }
+
+    static class SubCase {
+
+        private final int i, j, expected;
+
+        SubCase(int i, int j, int expected) {
+            this.i = i;
+            this.j = j;
+            this.expected = expected;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%d:%d] --> %d", i, j, expected);
+        }
+    }
+}
