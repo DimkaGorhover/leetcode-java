9417ea2b1aba3791a71fcbbafa60a4aee8f4060a
Dmytro_Horkhover <gd.mail.89@gmail.com>
2019-09-24 17:15:29 +0300

    LeetCode 208

---
+++ src/main/java/org/gd/leetcode/p0208/Trie.java
@@ -0,0 +1,108 @@
+package org.gd.leetcode.p0208;
+
+import org.gd.leetcode.common.LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @since 2019-09-24
+ */
+@LeetCode(difficulty = LeetCode.Level.MEDIUM, tags = {LeetCode.Tags.DESIGN, LeetCode.Tags.TRIE})
+class Trie {
+
+    private static final int NODES_LENGTH = 'z' - 'a' + 1;
+    private static final int WORD_MAX_LENGTH = 1 << 10;
+
+    private final Set<String> containsSet = new HashSet<>();
+    private final Node root = new Node((char) 0);
+
+    /** Initialize your data structure here. */
+    public Trie() {}
+
+    private static void checkWord(String word) {
+        requireNonNull(word, "\"word\" cannot be null");
+        final int length = word.length();
+        if (length > WORD_MAX_LENGTH)
+            throw new ToLongWordException(length);
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        checkWord(word);
+        containsSet.add(word);
+        root.insert(word, 0);
+    }
+
+    /**
+     * @return {@code true} if the word is in the trie.
+     */
+    public boolean search(String word) {
+        return containsSet.contains(word);
+    }
+
+    /**
+     * @return {@code true} if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        return root.startsWith(prefix, 0);
+    }
+
+    static class ToLongWordException extends RuntimeException {
+
+        ToLongWordException(int length) {
+            super(String.format("max length: %d, current length: %d", WORD_MAX_LENGTH, length));
+        }
+    }
+
+    static class Node {
+
+        private final char c;
+
+        private Node[] child;
+
+        Node(char c) { this.c = c; }
+
+        private static int index(char c) { return c - 'a'; }
+
+        /**
+         * @return {@code null} if child node doesn't exist
+         */
+        private Node findNode(char c) {
+            return child == null ? null : child[index(c)];
+        }
+
+        /**
+         * @return always return instance of child node
+         */
+        private Node getNode(char c) {
+            if (child == null)
+                child = new Node[NODES_LENGTH];
+            Node node = child[index(c)];
+            return node != null ? node : (child[index(c)] = new Node(c));
+        }
+
+        /**
+         * FIXME: it may raise {@link StackOverflowError} because it's de facto recursion. Dirty fix is in {@link Trie#insert(String)}
+         */
+        void insert(String text, int index) {
+            if (index < text.length())
+                getNode(text.charAt(index)).insert(text, (index + 1));
+        }
+
+        /**
+         * FIXME: it may raise {@link StackOverflowError} because it's de facto recursion. Dirty fix is in {@link Trie#insert(String)}
+         */
+        boolean startsWith(String prefix, int index) {
+            final Node node;
+            if (prefix == null || index >= prefix.length() || (node = findNode(prefix.charAt(index))) == null)
+                return false;
+            return (index == prefix.length() - 1) || node.startsWith(prefix, (index + 1));
+        }
+
+        @Override
+        public String toString() { return "Node{ " + c + " }"; }
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0208/TrieTest.java
@@ -0,0 +1,140 @@
+package org.gd.leetcode.p0208;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Trie}
+ *
+ * @since 2019-09-24
+ */
+@Execution(ExecutionMode.CONCURRENT)
+class TrieTest {
+
+    private static List<Map.Entry<String, String>> rawTestCases(String fileName) {
+        requireNonNull(fileName, "\"fileName\" cannot be null");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(TrieTest.class.getResourceAsStream(fileName)))) {
+
+            String ops = reader.readLine();
+            String values = reader.readLine();
+
+            List<String> opsList = Arrays.stream((ops.substring(1, ops.length() - 1)).split(","))
+                    .map(s -> s.substring(1, s.length() - 1))
+                    .collect(Collectors.toList());
+
+            List<String> valuesList = Arrays.stream(values.substring(1, values.length() - 1).split(","))
+                    .map(s -> s.length() > 2 ? s.substring(2, s.length() - 2) : "")
+                    .collect(Collectors.toList());
+
+            assertEquals(opsList.size(), valuesList.size());
+
+            List<Map.Entry<String, String>> map = new ArrayList<>(valuesList.size());
+            for (int i = 0; i < valuesList.size(); i++)
+                map.add(new AbstractMap.SimpleEntry<>(opsList.get(i), valuesList.get(i)));
+
+            return map;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String generateRandomWord(int wordLength) {
+        return ThreadLocalRandom.current().ints(wordLength, 'a', 'z')
+                .mapToObj(code -> (char) code)
+                .reduce(new StringBuilder(wordLength), StringBuilder::append, (sb, ignore) -> sb)
+                .toString();
+    }
+
+    private static Stream<Arguments> args() {
+
+        Stream<TestCase> stream0 = Stream.of("test_case_001.txt")
+                .flatMap(fileName -> rawTestCases(fileName).stream())
+                .flatMap(entry -> Stream.empty());
+
+        String word0 = generateRandomWord(1 << 10);
+
+        Stream<TestCase> stream1 = Stream.of(
+                TestCase.of(
+                        trie -> assertFalse(trie.search("apple")),
+                        trie -> assertFalse(trie.startsWith("app")),
+                        trie -> trie.insert("apple"),
+                        trie -> assertThrows(Trie.ToLongWordException.class, () -> trie.insert(generateRandomWord(1 << 12))),
+                        trie -> assertTrue(trie.search("apple")),
+                        trie -> assertFalse(trie.search("app")),
+                        trie -> assertTrue(trie.startsWith("app")),
+                        trie -> trie.insert("app"),
+                        trie -> assertTrue(trie.search("app")),
+                        trie -> trie.insert("applezzz"),
+                        trie -> trie.insert("zzz"),
+                        trie -> assertTrue(trie.search("zzz")),
+                        trie -> assertTrue(trie.startsWith("zzz")),
+                        trie -> assertTrue(trie.startsWith("zz")),
+                        trie -> trie.insert(word0),
+                        trie -> assertTrue(trie.search(word0)),
+                        trie -> assertTrue(trie.startsWith(word0.substring(0, word0.length() - 10))),
+                        trie -> assertFalse(trie.search(word0.substring(1)))
+                )
+        );
+
+        return Stream.concat(stream0, stream1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("main")
+    void test_main(TestCase testCase) {
+        final Trie trie = new Trie();
+        for (TestAssertion assertion : testCase)
+            assertion.doAssert(trie);
+    }
+
+    @FunctionalInterface
+    interface TestAssertion {
+
+        void doAssert(Trie trie);
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    static class TestCase implements Iterable<TestAssertion>, Arguments {
+
+        private final List<TestAssertion> testAssertions;
+
+        @Deprecated
+        static TestCase of(List<Map.Entry<String, String>> entries) {
+            requireNonNull(entries, "\"entry\" cannot be null");
+            return new TestCase(Collections.emptyList());
+        }
+
+        @Nonnull
+        static TestCase of(TestAssertion... arr) {
+            requireNonNull(arr, "\"arr\" cannot be null");
+            return new TestCase(Arrays.asList(arr));
+        }
+
+        @Nonnull
+        @Override
+        public Iterator<TestAssertion> iterator() {
+            return testAssertions.iterator();
+        }
+
+        @Override
+        public Object[] get() { return new Object[]{this}; }
+    }
+}
