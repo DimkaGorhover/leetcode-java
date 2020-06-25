8c11f4b6175039c91415d4f4767d082a6e26572f
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-06-25 16:49:50 +0300

    leetcode 211

---
+++ src/test/java/org/gd/leetcode/p0211/WordDictionaryTest.java
@@ -0,0 +1,60 @@
+package org.gd.leetcode.p0211;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link WordDictionary}
+ */
+class WordDictionaryTest {
+
+    @Test
+    @DisplayName("LeetCode #211: Word Dictionary (test #1)")
+    void test001() {
+
+        var dict = new WordDictionary();
+
+        assertFalse(dict.search("a"));
+        assertFalse(dict.search("abc"));
+
+        dict.addWord("helloworld");
+        dict.addWord("hecloworld");
+
+        assertFalse(dict.search("hello"));
+
+        assertFalse(dict.search("he.lo"));
+        assertTrue(dict.search("helloworld"));
+        assertTrue(dict.search("hell.world"));
+
+        assertFalse(dict.search("helloname"));
+    }
+
+    @Test
+    @DisplayName("LeetCode #211: Word Dictionary (test #2)")
+    void test002() {
+
+        var dict = new WordDictionary();
+
+        dict.addWord("at");
+        dict.addWord("and");
+        dict.addWord("an");
+        dict.addWord("add");
+
+        assertFalse(dict.search("a"));
+        assertFalse(dict.search(".at"));
+        
+        dict.addWord("bat");
+        
+        assertTrue(dict.search(".at"));
+        assertTrue(dict.search("an."));
+        assertFalse(dict.search("a.d."));
+
+        assertFalse(dict.search("b."));
+        assertTrue(dict.search("a.d"));
+        assertFalse(dict.search("."));
+    }
+
+}
