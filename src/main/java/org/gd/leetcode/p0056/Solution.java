9d57f52a20df554d1a2b5c1fbe6bcb8930d538db
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-07-06 10:51:24 +0300

    LeetCode 56

---
+++ src/main/java/org/gd/leetcode/p0056/Solution.java
@@ -0,0 +1,72 @@
+package org.gd.leetcode.p0056;
+
+import org.gd.leetcode.common.LeetCode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * https://leetcode.com/problems/merge-intervals/
+ *
+ * @author Horkhover D.
+ * @since 2020-07-06
+ */
+@LeetCode(
+        name = "Merge Intervals",
+        difficulty = LeetCode.Level.MEDIUM,
+        state = LeetCode.State.DONE,
+        tags = {
+                LeetCode.Tags.ARRAY,
+                LeetCode.Tags.SORT
+        })
+class Solution {
+
+    private static void sort(int[][] intervals) {
+        boolean result = true;
+        for (int next = 1, prev = 0; next < intervals.length; next++, prev++) {
+            int[] prevInterval = intervals[prev];
+            if (prevInterval[0] > intervals[next][0]) {
+                intervals[prev] = intervals[next];
+                intervals[next] = prevInterval;
+                result = false;
+                break;
+            }
+        }
+
+        if (!result)
+            Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+    }
+
+    private static int[][] trim(int[][] intervals, int length) {
+        if (intervals.length == length)
+            return intervals;
+
+        int[][] arr = new int[length][];
+        System.arraycopy(intervals, 0, arr, 0, length);
+        return arr;
+    }
+
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals == null)
+            throw new NullPointerException();
+
+        if (intervals.length <= 1)
+            return intervals;
+
+        sort(intervals);
+
+        int[][] mergedIntervals = new int[intervals.length][];
+        int j = 0;
+        mergedIntervals[j] = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (mergedIntervals[j][1] >= intervals[i][0]) {
+                mergedIntervals[j][1] = Math.max(mergedIntervals[j][1], intervals[i][1]);
+            } else {
+                mergedIntervals[++j] = intervals[i];
+            }
+        }
+
+        return trim(mergedIntervals, j + 1);
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0056/SolutionTest.java
@@ -0,0 +1,63 @@
+package org.gd.leetcode.p0056;
+
+import org.gd.common.CollectionUtils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ *
+ * @author Horkhover D.
+ * @since 2020-07-06.07.2020
+ */
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+
+                Arguments.of(
+                        new int[][]{{1, 3}, {0, 2}, {2, 3}, {4, 6}, {4, 5}, {5, 5}, {0, 2}, {3, 3}},
+                        new int[][]{{0, 3}, {4, 6}}
+                ),
+
+                Arguments.of(
+                        new int[][]{{2, 3}, {5, 5}, {2, 2}, {3, 4}, {3, 4}},
+                        new int[][]{{2, 4}, {5, 5}}
+                ),
+                Arguments.of(
+                        new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+                        new int[][]{{1, 6}, {8, 10}, {15, 18}}
+                ),
+                Arguments.of(
+                        new int[][]{{1, 4}, {4, 5}},
+                        new int[][]{{1, 5}}
+                ),
+                Arguments.of(
+                        new int[][]{{1, 4}, {2, 4}},
+                        new int[][]{{1, 4}}
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("Merge")
+    void test_Merge(int[][] intervals, int[][] expected) {
+
+        int[][] actual = new Solution().merge(intervals);
+
+        assertEquals(expected.length, actual.length);
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(
+                    CollectionUtils.listOf(expected[i]),
+                    CollectionUtils.listOf(actual[i]));
+        }
+    }
+}
