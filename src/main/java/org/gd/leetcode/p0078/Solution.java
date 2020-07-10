0bbe300b814a149652563424012f29fc228be677
DimkaGorhover <gd.mail.89@gmail.com>
2020-07-10 20:27:03 +0300

    LeetCode 78

---
+++ src/main/java/org/gd/leetcode/p0078/Solution.java
@@ -0,0 +1,74 @@
+package org.gd.leetcode.p0078;
+
+import org.gd.leetcode.common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/subsets/submissions/
+ */
+@LeetCode(
+        name = "Subsets",
+        difficulty = LeetCode.Level.MEDIUM,
+        state = LeetCode.State.DONE,
+        tags = {
+                LeetCode.Tags.ARRAY,
+                LeetCode.Tags.BACKTRACKING,
+                LeetCode.Tags.BIT_MANIPULATION
+        }
+)
+class Solution {
+
+    private int[] nums;
+    private List<List<Integer>> result;
+
+    private static List<Integer> copy(List<Integer> list, int value) {
+
+        if (list == null || list.isEmpty())
+            return Collections.singletonList(value);
+
+        List<Integer> newList = new ArrayList<>(list.size() + 1);
+        newList.addAll(list);
+        newList.add(value);
+        return Collections.unmodifiableList(newList);
+    }
+
+    private void reset(int[] nums) {
+        this.nums = nums;
+        this.result = new ArrayList<>(1 << nums.length);
+        result.add(Collections.emptyList());
+    }
+
+    private void populate(List<Integer> list, int i) {
+        List<Integer> copy;
+        for (int j = i; j < nums.length; j++) {
+            result.add(copy = copy(list, nums[j]));
+            populate(copy, j + 1);
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        if (nums == null)
+            return Collections.emptyList();
+
+        if (nums.length == 0)
+            return Collections.singletonList(Collections.emptyList());
+
+        if (nums.length == 1)
+            return Arrays.asList(
+                    Collections.emptyList(),
+                    Collections.singletonList(nums[0]));
+
+        reset(nums);
+        populate(null, 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return result == null ? "[]" : result.toString();
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0078/SolutionTest.java
@@ -0,0 +1,46 @@
+package org.gd.leetcode.p0078;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ */
+@DisplayName("LeetCode #78: Subsets")
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3}, List.of(
+                        List.of(3),
+                        List.of(1),
+                        List.of(2),
+                        List.of(1, 2, 3),
+                        List.of(1, 3),
+                        List.of(2, 3),
+                        List.of(1, 2),
+                        List.of()
+                ))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void subsets(int[] nums, List<List<Integer>> expected) {
+
+        var actual = new Solution().subsets(nums);
+
+        assertEquals(
+                new HashSet<>(expected),
+                new HashSet<>(actual));
+    }
+}
