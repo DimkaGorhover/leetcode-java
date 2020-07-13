14d3602fe4126ad39e2891c89ad6f2408022e428
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-07-13 16:16:36 +0300

    LeetCode 131: Palindrome Partitioning

---
+++ src/main/java/org/gd/leetcode/p0131/Solution.java
@@ -0,0 +1,79 @@
+package org.gd.leetcode.p0131;
+
+import org.gd.leetcode.common.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Horkhover D.
+ * @since 2020-07-13.07.2020
+ */
+@LeetCode(
+        name = "Palindrome Partitioning",
+        difficulty = LeetCode.Level.MEDIUM,
+        state = LeetCode.State.DONE,
+        tags = {
+                LeetCode.Tags.BACKTRACKING
+        }
+)
+class Solution {
+
+    private List<List<String>> result;
+
+    private static boolean isPalindrome(String word) {
+        final int length = word.length();
+        if (length == 0)
+            return false;
+        if (length == 1)
+            return true;
+        int mid = length >> 1;
+        for (int i = 0; i <= mid; i++) {
+            int j = length - 1 - i;
+            if (word.charAt(i) != word.charAt(j))
+                return false;
+        }
+        return true;
+    }
+
+    private static List<String> add(List<String> list, String word) {
+        if (list == null || list.isEmpty())
+            return Collections.singletonList(word);
+
+        if (list.size() == 1)
+            return Arrays.asList(list.get(0), word);
+
+        ArrayList<String> newList = new ArrayList<>(list.size() + 1);
+        newList.addAll(list);
+        newList.add(word);
+        return Collections.unmodifiableList(newList);
+    }
+
+    private void partition(final List<String> list,
+                           final String word) {
+
+        final int wordLength = word.length();
+        if (wordLength == 0) {
+            result.add(list);
+            return;
+        }
+        if (wordLength == 1) {
+            result.add(add(list, word));
+            return;
+        }
+
+        for (int i = 1; i <= wordLength; i++) {
+            String ss = word.substring(0, i);
+            if (isPalindrome(ss))
+                partition(add(list, ss), word.substring(i, wordLength));
+        }
+    }
+
+    public List<List<String>> partition(String word) {
+        result = new ArrayList<>();
+        partition(Collections.emptyList(), word);
+        return result;
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0131/SolutionTest.java
@@ -0,0 +1,48 @@
+package org.gd.leetcode.p0131;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ *
+ * @author Horkhover D.
+ * @since 2020-07-13
+ */
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of("caba", List.of(
+                        List.of("c", "aba"),
+                        List.of("c", "a", "b", "a")
+                )),
+                Arguments.of("aab", List.of(
+                        List.of("aa", "b"),
+                        List.of("a", "a", "b")
+                ))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("Partition")
+    void test_Partition(String word, List<List<String>> expected) {
+
+        List<List<String>> actual = new Solution().partition(word);
+
+        assertEquals(expected.size(), actual.size());
+
+        for (List<String> strings : expected) {
+            assertTrue(actual.contains(strings));
+        }
+
+    }
+}
