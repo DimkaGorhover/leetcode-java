e1476dbb1fa340cab9b62e62e4346e39543b3455
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-07-17 19:24:51 +0300

    LeetCode 965: Univalued Binary Tree

---
+++ src/main/java/org/gd/leetcode/p0965/Solution.java
@@ -0,0 +1,28 @@
+package org.gd.leetcode.p0965;
+
+import org.gd.leetcode.common.LeetCode;
+import org.gd.leetcode.common.TreeNode;
+
+/**
+ * https://leetcode.com/problems/univalued-binary-tree/
+ */
+@LeetCode(
+        name = "Univalued Binary Tree",
+        difficulty = LeetCode.Level.EASY,
+        state = LeetCode.State.TODO,
+        tags = {
+                LeetCode.Tags.TREE
+        }
+)
+class Solution {
+
+    private static boolean isUnivalTree(TreeNode root, final int value) {
+        return root == null || (root.val == value
+                && isUnivalTree(root.left, value)
+                && isUnivalTree(root.right, value));
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        return root == null || isUnivalTree(root, root.val);
+    }
+}
