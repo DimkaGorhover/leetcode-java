5c16cca2f68c5b12168e2a5e3c2dccdda9813901
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-07-27 17:15:09 +0300

    dev

---
+++ src/main/java/org/gd/leetcode/p0033/Solution.java
@@ -0,0 +1,84 @@
+package org.gd.leetcode.p0033;
+
+import org.gd.leetcode.common.LeetCode;
+
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ *
+ * @author Horkhover Dmytro
+ * @see org.gd.leetcode.p0153.Solution
+ * @see org.gd.leetcode.p0154.Solution
+ * @see java.util.Arrays#binarySearch(int[], int)
+ * @since 2020-07-27
+ */
+@SuppressWarnings("JavadocReference")
+@LeetCode(
+        name = "Search in Rotated Sorted Array",
+        difficulty = LeetCode.Level.MEDIUM,
+        state = LeetCode.State.TODO,
+        tags = {
+                LeetCode.Tags.ARRAY,
+                LeetCode.Tags.BINARY_SEARCH
+        }
+)
+class Solution {
+
+    private static int linearSearch(int[] nums, int target) {
+        if (nums.length == 0)
+            return -1;
+
+        if (nums.length == 1)
+            return nums[0] == target ? 0 : -1;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == target)
+                return i;
+
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+
+        if (nums == null)
+            return -1;
+
+        if (nums.length < 5)
+            return linearSearch(nums, target);
+
+        int left = 0;
+        int mid;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            final int midValue = nums[mid = (left + right) >>> 1];
+            final int leftValue = nums[left];
+            final int rightValue = nums[right];
+
+            if (midValue == target) return mid;
+            if (leftValue == target) return left;
+            if (rightValue == target) return right;
+
+            if (leftValue > midValue) {
+
+                if (target < midValue || leftValue > target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            } else {
+
+                if (leftValue < target && target < midValue) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0033/SolutionTest.java
@@ -0,0 +1,47 @@
+package org.gd.leetcode.p0033;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ *
+ * @author Horkhover Dmytro
+ * @since 2020-07-27
+ */
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+
+
+        int[] ints = IntStream.concat(
+                IntStream.rangeClosed(101, 1000),
+                IntStream.rangeClosed(1, 100)
+        ).toArray();
+
+        return Stream.of(
+                Arguments.of(Arrays.copyOf(ints, ints.length), 13, 912),
+                Arguments.of(new int[]{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}, 25, 4),
+                Arguments.of(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 1}, 9, 7),
+                Arguments.of(new int[]{4, 5, 6, 7, 0, 1, 2}, 3, -1),
+                Arguments.of(new int[]{4, 5, 6, 7, 0, 1, 2}, 0, 4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("Search")
+    //@Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    void test_Search(int[] nums, int target, int expected) {
+        assertEquals(expected, new Solution().search(nums, target));
+    }
+}
