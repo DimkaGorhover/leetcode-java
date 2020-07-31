c73f454c2e0daa076ab98b95242bd4b041070625
DimkaGorhover <gd.mail.89@gmail.com>
2020-07-31 14:14:17 +0300

    LeetCode 200: Number of Islands

---
+++ src/main/java/org/gd/leetcode/p0200/Solution.java
@@ -0,0 +1,44 @@
+package org.gd.leetcode.p0200;
+
+import org.gd.leetcode.common.LeetCode;
+
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ *
+ * @author Horkhover Dmytro
+ * @since 2020-07-31
+ */
+@LeetCode(
+        name = "Number of Islands",
+        difficulty = LeetCode.Level.MEDIUM,
+        state = LeetCode.State.DONE,
+        tags = {
+                LeetCode.Tags.DEPTH_FIRST_SEARCH,
+                LeetCode.Tags.BREADTH_FIRST_SEARCH,
+                LeetCode.Tags.UNION_FIND
+        }
+)
+class Solution {
+
+    public int numIslands(char[][] grid) {
+
+        final int rows;
+        final int cols;
+
+        if (grid == null || (rows = grid.length) == 0 || (cols = grid[0].length) == 0)
+            return 0;
+
+        final Cleaner cleaner = (rows * cols > (1 << 5)) ? new QueueCleaner(grid) : new RecursiveCleaner(grid);
+
+        int count = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] == '1') {
+                    cleaner.clean(row, col);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/org/gd/leetcode/p0200/SolutionTest.java
@@ -0,0 +1,80 @@
+package org.gd.leetcode.p0200;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.gd.common.ArrayUtils.copy;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ *
+ * @author Horkhover Dmytro
+ * @since 2020-07-31
+ */
+@DisplayName("LeetCode #200: Number of Islands")
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+
+                Arguments.of(new char[][]{
+                        {'1', '1', '0', '0', '0'},
+                        {'1', '1', '0', '0', '0'},
+                        {'0', '0', '1', '0', '0'},
+                        {'0', '0', '0', '1', '1'}
+                }, 3),
+
+                Arguments.of(new char[][]{
+                        {'1', '1', '1', '1', '0'},
+                        {'1', '1', '0', '1', '0'},
+                        {'1', '1', '0', '0', '0'},
+                        {'0', '0', '0', '0', '0'}
+                }, 1),
+
+                Arguments.of(new char[][]{
+                        {'1', '1', '1', '1', '1', '1', '0'},
+                        {'1', '0', '0', '0', '0', '1', '0'},
+                        {'1', '1', '0', '1', '1', '1', '0'},
+                        {'0', '1', '0', '0', '0', '0', '0'},
+                        {'1', '1', '0', '0', '0', '0', '0'},
+                        {'0', '0', '0', '0', '0', '0', '1'}
+                }, 2),
+
+                Arguments.of(new char[][]{
+                        {'1', '1', '1', '1', '1', '1', '0'},
+                        {'1', '0', '0', '0', '0', '1', '0'},
+                        {'1', '1', '0', '1', '1', '1', '0'},
+                        {'0', '1', '0', '0', '0', '0', '0'},
+                        {'1', '1', '0', '0', '0', '0', '0'},
+                        {'0', '0', '0', '0', '1', '0', '0'},
+                        {'0', '0', '0', '0', '1', '0', '1'},
+                        {'0', '0', '0', '0', '1', '1', '1'}
+                }, 2),
+
+                Arguments.of(new char[][]{
+                        {'1', '1', '1', '1', '1', '1', '0', '0', '0'},
+                        {'1', '0', '0', '0', '0', '1', '0', '0', '0'},
+                        {'1', '1', '0', '1', '1', '1', '0', '0', '0'},
+                        {'0', '1', '0', '0', '0', '0', '0', '0', '0'},
+                        {'1', '1', '0', '0', '0', '0', '1', '1', '1'},
+                        {'0', '0', '0', '0', '1', '0', '1', '0', '1'},
+                        {'0', '0', '0', '0', '1', '0', '1', '1', '1'},
+                        {'0', '0', '0', '0', '1', '1', '1', '1', '1'}
+                }, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("Number of Islands")
+    @Timeout(value = 50, unit = TimeUnit.MILLISECONDS)
+    void test_NumIslands(char[][] grid, int expected) {
+        assertEquals(expected, new Solution().numIslands(copy(grid)));
+    }
+}
