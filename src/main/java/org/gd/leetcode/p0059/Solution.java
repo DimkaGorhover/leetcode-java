69cde4e464790cdac6f9930258e054c8954fb21d
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-08-10 20:44:37 +0300

    LeetCode 59: Spiral Matrix II

--- src/main/java/org/gd/leetcode/p0059/Solution.java
+++ src/main/java/org/gd/leetcode/p0059/Solution.java
@@ -14,12 +14,40 @@ import org.gd.leetcode.common.LeetCode;
 @LeetCode(
         name = "Spiral Matrix II",
         difficulty = LeetCode.Level.MEDIUM,
-        state = LeetCode.State.TODO,
+        state = LeetCode.State.DONE,
         tags = LeetCode.Tags.ARRAY
 )
 class Solution {
 
     public int[][] generateMatrix(int n) {
-        throw new UnsupportedOperationException(new String(new char[]{ 175, 92, 95, 40, 12_484, 41, 95, 47, 175 }));
+        if (n < 0) throw new IndexOutOfBoundsException();
+        if (n == 0) return new int[0][0];
+        if (n == 1) return new int[][]{{1}};
+        if (n == 2) return new int[][]{{1, 2}, {4, 3}};
+
+        final int size = n * n;
+        final int mid = n >>> 1;
+        final int[][] matrix = new int[n][n];
+
+        int offset = 0;
+        int counter = 0;
+        while (offset <= mid) {
+
+            for (int col = offset; col < n - offset && counter < size; col++)
+                matrix[offset][col] = ++counter;
+
+            for (int row = offset + 1; row < n - offset - 1 && counter < size; row++)
+                matrix[row][n - offset - 1] = ++counter;
+
+            for (int col = n - offset - 1; col >= offset && counter < size; col--)
+                matrix[n - offset - 1][col] = ++counter;
+
+            for (int row = n - offset - 2; row > offset && counter < size; row--)
+                matrix[row][offset] = ++counter;
+
+            offset++;
+        }
+
+        return matrix;
     }
 }
---
+++ src/test/java/org/gd/leetcode/p0059/SolutionTest.java
@@ -0,0 +1,57 @@
+package org.gd.leetcode.p0059;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.gd.common.CollectionUtils.listOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link Solution}
+ *
+ * @author Horkhover Dmytro
+ * @since 2020-08-10
+ */
+@DisplayName("LeetCode #59: Spiral Matrix II")
+@Timeout(value = 100, unit = java.util.concurrent.TimeUnit.MILLISECONDS)
+class SolutionTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+
+                Arguments.of(5, new int[][]{
+                        {1, 2, 3, 4, 5},
+                        {16, 17, 18, 19, 6},
+                        {15, 24, 25, 20, 7},
+                        {14, 23, 22, 21, 8},
+                        {13, 12, 11, 10, 9}
+                }),
+
+                Arguments.of(4, new int[][]{
+                        {1, 2, 3, 4},
+                        {12, 13, 14, 5},
+                        {11, 16, 15, 6},
+                        {10, 9, 8, 7}
+                }),
+
+                Arguments.of(3, new int[][]{
+                        {1, 2, 3},
+                        {8, 9, 4},
+                        {7, 6, 5}
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    @DisplayName("GenerateMatrix")
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    void test_GenerateMatrix(int n, int[][] expected) {
+        assertEquals(listOf(expected), listOf(new Solution().generateMatrix(n)));
+    }
+}
