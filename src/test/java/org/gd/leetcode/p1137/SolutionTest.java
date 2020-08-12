571e5c20c7c37c821ea7bc7378f7e8493965b453
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-08-12 18:42:45 +0300

    dev

--- src/test/java/org/gd/leetcode/p1137/SolutionTest.java
+++ src/test/java/org/gd/leetcode/p1137/SolutionTest.java
@@ -1,25 +1,41 @@
 package org.gd.leetcode.p1137;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test for {@link Solution}
+ */
+@SuppressWarnings("SpellCheckingInspection")
+@DisplayName("LeetCode #1137: N-th Tribonacci Number")
 class SolutionTest {
 
-    @Test
-    void tribonacci() {
-        for (int i = 0; i < 10; i++) {
-            int value = new Solution().tribonacci(i);
-            System.out.printf("%d --> %d%n", i, value);
-        }
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(0, 0),
+                Arguments.of(1, 1),
+                Arguments.of(2, 1),
+                Arguments.of(3, 2),
+                Arguments.of(4, 4),
+                Arguments.of(5, 7),
+                Arguments.of(6, 13),
+                Arguments.of(7, 24),
+                Arguments.of(8, 44),
+                Arguments.of(9, 81)
+        );
+    }
+
+    @ParameterizedTest(name = "tribonacci({0}) = {1}")
+    @MethodSource("args")
+    @Timeout(value = 20, unit = TimeUnit.MILLISECONDS)
+    void tribonacci(int n, int expected) {
+        assertEquals(expected, new Solution().tribonacci(n));
     }
 }
-
-/*
-0
-1
-1
-2
-4
-7
- */
