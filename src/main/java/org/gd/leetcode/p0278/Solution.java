0fb352bf9d55847e7e6edad778dbc853bf3641a6
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-08-15 14:09:00 +0300

    dev

---
+++ src/main/java/org/gd/leetcode/p0278/Solution.java
@@ -0,0 +1,10 @@
+package org.gd.leetcode.p0278;
+
+/**
+ * https://leetcode.com/problems/first-bad-version/
+ *
+ * @author Horkhover Dmytro
+ * @since 2020-08-15
+ */
+class Solution extends VersionControl {
+}
---
+++ src/main/java/org/gd/leetcode/p0278/VersionControl.java
@@ -0,0 +1,12 @@
+package org.gd.leetcode.p0278;
+
+/**
+ * @author Horkhover Dmytro
+ * @since 2020-08-15
+ */
+class VersionControl {
+
+    boolean isBadVersion(int version) {
+        throw new UnsupportedOperationException(new String(new char[]{ 175, 92, 95, 40, 12_484, 41, 95, 47, 175 }));
+    }
+}
