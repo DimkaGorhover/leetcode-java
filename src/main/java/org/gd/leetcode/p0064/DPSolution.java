f088f9679875f65f12f8007a8b8453f1ba368d92
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-08-15 23:16:05 +0300

    dev

--- src/main/java/org/gd/leetcode/p0064/DPSolution.java
+++ src/main/java/org/gd/leetcode/p0064/DPSolution.java
@@ -15,19 +15,15 @@ class DPSolution implements Solution {
         if (cols == 0)
             return 0;
 
-        for (int row = 1; row < rows; row++) {
+        for (int row = 1; row < rows; row++)
             grid[row][0] += grid[row - 1][0];
-        }
 
-        for (int col = 1; col < cols; col++) {
+        for (int col = 1; col < cols; col++)
             grid[0][col] += grid[0][col - 1];
-        }
 
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                grid[i][j] = Math.min(grid[i][j] + grid[i - 1][j], grid[i][j] + grid[i][j - 1]);
-            }
-        }
+        for (int row = 1; row < rows; row++)
+            for (int col = 1; col < cols; col++)
+                grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
 
         return grid[rows - 1][cols - 1];
     }
